a56e189e9faf9c46a937c5128f259c7f884c38e8
roncoleman125 <ronncoleman@gmail.com>
2019-05-10 15:05:30 -0400

    Added tests for cosine similarity.

---
+++ src/Cosine.scala
@@ -0,0 +1,18 @@
+import scala.io.Source
+
+object Cosine extends App {
+  import org.apache.commons.text.similarity.CosineDistance
+
+  val distance = new CosineDistance
+
+  val astyle = "Astyle --style=linux < linux-kernel/acct.c > acct.c.linux"
+  val cmd = Array("/bin/sh", "-c", astyle)
+  Runtime.getRuntime.exec(cmd)
+
+  val base = Source.fromFile("/users/roncoleman/tmp/style/linux-kernel/arraymap.c").mkString
+  val treated = Source.fromFile("/users/roncoleman/tmp/style/acct.c.linux").mkString
+
+  val d = distance.apply(treated, base)
+  println(d)
+
+}
